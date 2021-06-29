6511cd16c9b492bd15faa55e68456d4fb9239e4f
Marcelinaaaa <marcelina808@poczta.onet.pl>
2021-06-29 15:39:44 +0200

    Bridge and Facade

---
+++ Most - pobieranie tresci/most.py
@@ -0,0 +1,77 @@
+import abc
+import os.path
+import urllib.request
+
+
+class ResourceContent:
+    """
+    Define the abstraction's interface.
+    Maintain a reference to an object which represents the Implementor.
+    """
+
+    def __init__(self, imp):
+        self._imp = imp
+
+    def show_content(self, path):
+            self._imp.fetch(path)
+
+
+class ResourceContentFetcher(metaclass=abc.ABCMeta):
+    """
+    Define the interface (Implementor) for implementation classes that help fetch content.
+    """
+
+    @abc.abstractmethod
+    def fetch(path):
+        pass
+
+
+class URLFetcher(ResourceContentFetcher):
+    """
+    Implement the Implementor interface and define its concrete implementation.
+    """
+
+    def fetch(self, path):
+        # path is an URL
+        try:
+            req = urllib.request.Request(path)
+            with urllib.request.urlopen(req) as response:
+                if response.code == 200:
+                    the_page = response.read()
+                    print(the_page)
+        except:
+            print(f"Błędny adres URL {path}")
+
+
+class LocalFileFetcher(ResourceContentFetcher):
+    """
+    Implement the Implementor interface and define its concrete implementation.
+    """
+
+    def fetch(self, path):
+        # path is the filepath to a text file
+        if os.path.isfile(path):
+            with open(path) as f:
+                print(f.read())
+        else:
+            print(f"Plik {path} nie istnieje")
+
+
+def main():
+    url_fetcher = URLFetcher()
+    iface = ResourceContent(url_fetcher)
+    iface.show_content('http://python.org')
+    print('===================')
+    iface.show_content('http://pythonnnn.org')
+
+    print('===================')
+
+    localfs_fetcher = LocalFileFetcher()
+    iface = ResourceContent(localfs_fetcher)
+    iface.show_content('file.txt')
+    print('===================')
+    iface.show_content('file.txt')
+
+
+if __name__ == "__main__":
+    main()
